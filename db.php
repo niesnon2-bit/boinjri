2c7d7bbac0ead61bf21decf54fe51bafdcde3281
niesnon2-bit <niesnon2@gmail.com>
2026-04-29 06:14:58 +0300

    Update db.php

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 // عدّل القيم حسب استضافتك/قاعدة البيانات
 define('APP_BASE', '');
 
-define('DB_HOST', getenv('MYSQLHOST') ?: 'mysql.railway.internal');
+define('DB_HOST', 'mysql.railway.internal:3306');
 define('DB_NAME', getenv('MYSQLDATABASE') ?: 'railway');
 define('DB_USER', getenv('MYSQLUSER') ?: 'root');
 define('DB_PASS', getenv('MYSQLPASSWORD') ?: 'mihiGRVvutPwuuyPkXLQGSoYRWAhwGH');
